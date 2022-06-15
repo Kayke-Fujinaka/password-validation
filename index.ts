07833621c4dbae13427fc2b0914ceff2171ef29c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-15 17:31:11 -0300

    First Commit

---
+++ index.ts
@@ -0,0 +1,75 @@
+const password = "1234567812345678!@";
+
+// Eles estão armazenas, pois facilitarão caso precise alterar alguma exigência
+
+// Variável para armazenar os números de 0 a 9
+const numbers = "0123456789";
+
+// Variáveis para armazenar o menor e o maior tamanho da senha
+const minLength = 16;
+const maxLength = 32;
+
+// Variável para armazenar os caracteres especiais
+const specialCharacters =
+  '&!?@#$*+%=.,/\\[]{}();: ¨ ^~¨´`ç"“”‘’‚„‹›<>«»ºª–­-_  ¯…¦•‣¶§';
+
+export type Response = {
+  result: boolean;
+  error: string[];
+};
+
+const numberValid = (password: string) => {
+  const numberValid = password.split("").filter((e) => numbers.includes(e));
+  if (numberValid.length < 1) {
+    return false;
+  }
+  return true;
+};
+
+const lengthValid = (password: string): boolean => {
+  return password.length <= minLength || password.length >= maxLength
+    ? false
+    : true;
+};
+
+const specialCharactersValid = (password: string) => {
+  const specialValid = password
+    .split("")
+    .filter((e) => specialCharacters.includes(e));
+  if (specialValid.length < 2) {
+    return false;
+  }
+  return true;
+};
+
+const passwordValid = (password: string): object => {
+  const errorsResult: Response = {
+    result: true,
+    error: [],
+  };
+
+  if (!numberValid(password)) {
+    errorsResult.error.push("A senha deve conter um número");
+    errorsResult.result = false;
+  }
+
+  if (!lengthValid(password)) {
+    errorsResult.error.push("A senha deve conter de 16 a 32 caracteres");
+    errorsResult.result = false;
+  }
+
+  if (!specialCharactersValid(password)) {
+    errorsResult.error.push("A senha deve conter 2 caracteres especiais");
+    errorsResult.result = false;
+  }
+
+  return errorsResult;
+};
+
+const callFunction = passwordValid(password);
+console.log(callFunction);
+
+console.log(specialCharactersValid(password));
+
+// npm run build - Conversão entre Typescript e Javascript
+// node index - Para executar
